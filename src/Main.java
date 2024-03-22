08c2d5f6d90ce6444eea78a77e3d812aea4c2f88
Usee <yasno.ponyatno229@gmail.com>
2024-03-22 16:18:15 +0300

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static void main(String[] args) {
 
+        int price = 12_400;
+
+        int miles = price / 20;
+        System.out.println("Бонус: " + miles);
+    }
 }
